f2827bcf97ed8baf5972d48fe7ee16c1d4fec2e2
Omar Mojaddidi <omar.m.mojaddidi@gmail.com>
2024-10-10 15:44:53 -0400

    Edited name

---
+++ src/main/java/edu/gmu/cs321/Immigrant.java
@@ -0,0 +1,12 @@
+package edu.gmu.cs321;
+
+public class Immigrant {
+
+    private int id=3;
+
+    public int getID()
+{
+    return this.id;
+}
+
+}
---
+++ src/test/java/edu/gmu/cs321/ImmigrantTest.java
@@ -0,0 +1,13 @@
+package edu.gmu.cs321;
+import org.junit.Test;
+
+public class ImmigrantTest
+{
+    @Test
+    public void testwhatEver()
+    {
+        Immigrant testImm =new Immigrant();
+        int testID = testImm.getID();
+        //assertTrue();
+    }
+}
