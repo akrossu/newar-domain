9a8cbcf875a0aad6f4fae888e1451df6af2b270e
Omar Mojaddidi <omar.m.mojaddidi@gmail.com>
2024-10-21 20:21:09 -0400

    Update ImmFormTest.java

--- src/test/java/edu/gmu/cs321/ImmFormTest.java
+++ src/test/java/edu/gmu/cs321/ImmFormTest.java
@@ -5,5 +5,13 @@ import org.junit.jupiter.api.Test;
 
 public class ImmFormTest
 {
+    @Test
+    public void testValidateForm
+    {
+        Immigrant testImm=new Immigrant();
+        //Relative testRel=new Relative();
+        ImmForm testImmForm= new ImmForm(testImm, null, null, null);
+        assertTrue(testImmForm.validateForm());
+    }
 
 }
