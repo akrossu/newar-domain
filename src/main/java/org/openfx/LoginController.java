b6db5e5ad872cfc7f6c1e26853bb5c6a7ef9f88c
Akross <59298840+akrossu@users.noreply.github.com>
2024-11-04 11:54:00 -0500

    basic scene switcheer

--- src/main/java/org/openfx/LoginController.java
+++ src/main/java/org/openfx/LoginController.java
@@ -12,4 +12,27 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LoginController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    public void onDataEntryButtonClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("dataEntry-view.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Data Entry");
+        stage.show();
+    }
+
+    @FXML
+    public void onLogoutButtonClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login-view.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Data Entry");
+        stage.show();
+    }
 }
