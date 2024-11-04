6f6f7eeafd901b360a1641e17e9d698a1ddc854b
Akross <59298840+akrossu@users.noreply.github.com>
2024-11-04 13:04:32 -0500

    added review/approve button template

--- src/main/java/org/openfx/LoginController.java
+++ src/main/java/org/openfx/LoginController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,23 +15,50 @@ public class LoginController {
     private Scene scene;
     private Parent root;
 
-    @FXML
-    public void onDataEntryButtonClick(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("dataEntry-view.fxml"));
+    private void createScene(ActionEvent event, String file) throws IOException {
+        root = FXMLLoader.load(getClass().getResource(file + "-view.fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
+    }
+
+    @FXML
+    public void onDataEntryButtonClick(ActionEvent event) throws IOException {
+        createScene(event, "dataEntry");
         stage.setTitle("Data Entry");
         stage.show();
     }
 
     @FXML
-    public void onLogoutButtonClick(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("login-view.fxml"));
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setTitle("Data Entry");
+    public void onReviewButtonClick(ActionEvent event) throws IOException {
+        createScene(event, "reviewer");
+        stage.setTitle("Reviewer");
         stage.show();
+    }
+
+    @FXML
+    public void onApproveButtonClick(ActionEvent event) throws IOException {
+        createScene(event, "approver");
+        stage.setTitle("Approver");
+        stage.show();
+    }
+
+    @FXML
+    public void onLogoutButtonClick(ActionEvent event) throws IOException {
+        createScene(event, "login");
+        stage.setTitle("Login");
+        stage.show();
+    }
+
+    public boolean onVerifyButtonClick(ActionEvent event) {
+        return true;
+    }
+
+    public boolean onSaveButtonClick(ActionEvent event) {
+        return true;
+    }
+
+    public Scene getScene() {
+        return scene;
     }
 }
