b640a166412559cca0522b4320b6ba90b45ab2a3
Omar Mojaddidi <omar.m.mojaddidi@gmail.com>
2024-11-14 00:09:23 -0500

    Reformed Approval Interface to FXML

---
+++ src/main/java/org/openfx/Approval.java
@@ -0,0 +1,22 @@
+package org.openfx;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Approval extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("approver-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Approval");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
