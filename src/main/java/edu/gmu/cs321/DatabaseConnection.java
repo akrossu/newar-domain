c5e1d4e04d915550caf73b9e8329cf28adccb7fd
Akross <59298840+akrossu@users.noreply.github.com>
2024-11-18 14:36:01 -0500

    initial commit

--- src/main/java/edu/gmu/cs321/DatabaseConnection.java
+++ src/main/java/edu/gmu/cs321/DatabaseConnection.java
@@ -1,6 +1,41 @@
 package edu.gmu.cs321;
 
-public class DatabaseConnection
-{
+import java.sql.*;
 
+public class DatabaseConnection {
+    ImmForm immForm;
+    private String DB_URL;
+    private String USER;
+    private String PASS;
+
+    DatabaseConnection() {}
+
+    public void setDbUrl(String dbUrl) {
+        this.DB_URL = dbUrl;
+    }
+
+    public void setUser(String user) {
+        this.USER = user;
+    }
+
+    public void setPassword(String pass) {
+        this.PASS = pass;
+    }
+
+    public Object[] queryDatabase(String query) {
+        try (Connection connection = DriverManager.getConnection(this.DB_URL, this.USER, this.PASS)) {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            rs.next();
+            return new Object[]{
+                    rs.getInt(1), //ID
+                    rs.getInt(2), //AGE
+                    rs.getString(3), //FIRST
+                    rs.getString(4) //LAST
+            };
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
