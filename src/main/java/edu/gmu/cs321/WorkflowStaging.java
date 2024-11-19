4a063c285010c74b0590b4197e65a2f1842849a6
Akross <59298840+akrossu@users.noreply.github.com>
2024-11-19 03:34:08 -0500

    added createNewWFItem function

--- src/main/java/edu/gmu/cs321/WorkflowStaging.java
+++ src/main/java/edu/gmu/cs321/WorkflowStaging.java
@@ -1,23 +1,38 @@
 package edu.gmu.cs321;
 
 import com.cs321.Workflow;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.SQLException;
 
 public class WorkflowStaging {
-    private static final Workflow workflow=new Workflow();
+    private static final Workflow workflow = new Workflow();
+    private final String approve = "Approve";
+    private final String review = "Review";
 
     public int addApproval(int id) {
-            return WorkflowStaging.workflow.AddWFItem(id, "Approve");
+        return WorkflowStaging.workflow.AddWFItem(id, approve);
     }
 
     public int addReviewer(int id) {
-        return WorkflowStaging.workflow.AddWFItem(id, "Review");
+        return WorkflowStaging.workflow.AddWFItem(id, review);
     }
 
     public int getNextApprovalWFItem() {
-        return WorkflowStaging.workflow.GetNextWFItem("Approve");
+        return WorkflowStaging.workflow.GetNextWFItem(approve);
     }
 
     public int getNextReviewerWFItem() {
-        return WorkflowStaging.workflow.GetNextWFItem("Review");
+        return WorkflowStaging.workflow.GetNextWFItem(review);
+    }
+
+    public void createNewWFItem(ImmForm form) {
+        Dotenv dotenv = Dotenv.configure().load();
+        DatabaseConnection dc = new DatabaseConnection();
+        dc.setUser(dotenv.get("USER"));
+        dc.setDbUrl(dotenv.get("DB_URL"));
+        dc.setPassword(dotenv.get("PASS"));
+        dc.insertIntoDatabase(form);
+        this.addApproval(form.getId());
     }
 }
