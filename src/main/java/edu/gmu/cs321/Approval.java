638690336b9a6c33bfb8e37264bc5943c997b907
Omar Mojaddidi <omar.m.mojaddidi@gmail.com>
2024-11-19 11:56:28 -0500

    Approval Work
    Completed major approval workflow and database work

--- src/main/java/edu/gmu/cs321/Approval.java
+++ src/main/java/edu/gmu/cs321/Approval.java
@@ -1,5 +1,10 @@
 package edu.gmu.cs321;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 /**
  * Approval responsible for approving
  */
@@ -8,27 +13,97 @@ public class Approval
     /**
      * object variables
      */
-    private ImmForm ImmForm;
+    private ImmForm immFormData;
+
+    private WorkflowStaging ourWorkflow;
+
+    private DatabaseConnection dc;
+
+    private Dotenv dotenv;
 
     /**
-     * validate Fields
+     * For testing, generally to find petitions we need to search for database for pending petitions.
+     */
+    public Approval()
+    {
+        ourWorkflow= new WorkflowStaging();
+        int id = 1;
+        ourWorkflow.addApproval(1);
+        ourWorkflow.addApproval(2);
+        dotenv = Dotenv.configure().load();
+        dc = new DatabaseConnection();
+        dc.setUser(dotenv.get("USER"));
+        dc.setDbUrl(dotenv.get("DB_URL"));
+        dc.setPassword(dotenv.get("PASS"));
+        //approval should start up and get the first workflow item, when opening.
+        this.getWorkflowForm();
+    }
+
+
+    /**
+     * sends an email.
+     * This function is to be completed at a later date, in case email compatibility becomes a 
+     * major requirement.
      */
     public Boolean sendEmail()
     {
         return false;
     }
     /**
-     * get workflowForm
-     * Currently returns nothing, but is currently being discussed for change.
+     * get workflowForm, set up our ImmFormData.
+     * acts as a setting for ImmForm
      */
     public void getWorkflowForm()
     {
+        ImmRelative tempRel;
+        Immigrant tempImm;
+        Date date1, date2;
+        int id = ourWorkflow.getNextApprovalWFItem(); 
+        //what happens if we cant get workflow?
+        immFormData=null;
+        if (id<0)
+        {
+            return;
+        }
+        Object[] obj = dc.queryDatabase("SELECT * FROM People WHERE id=" + id);
+        date1=(Date)obj[2];
+        date2=(Date)obj[6];
+        tempImm = new Immigrant.Builder<>()
+            .setName(obj[1].toString())
+            .setDateOfBirth(date1)
+            .setSSN((obj[3].toString()))
+            .build();
+
+        tempRel = new ImmRelative.Builder<>()
+                .setName(obj[5].toString())
+                .setRelationship("Relative")
+                .setDateOfBirth(date2)
+                .setNationality(obj[7].toString())
+                .setAlienRegistrationNumber(obj[8].toString())
+                .build();
+
+        String creationDate = LocalDateTime.now().toString();
+        String status = obj[4].toString();
+        immFormData = new ImmForm(id, tempImm, tempRel, creationDate, status);
     }
     /**
-     * add a worflow form
+     * add a worflow form, to review.
+     * After adding to Review, we should get our new Workflow Form.
      */
     public Boolean updateWorkflowForm()
     {
-        return false;
+        if (immFormData==null)
+        {
+            return false;
+        }
+        ourWorkflow.addReviewer(immFormData.getId());
+        //for now set to null but change to getting next in workflow
+
+        return true;
     }
+
+    public ImmForm getImmForm() {
+        return immFormData;
+    }
+
 }
--- src/main/java/org/openfx/ApprovalController.java
+++ src/main/java/org/openfx/ApprovalController.java
@@ -1,11 +1,13 @@
 
 package org.openfx;
 
+import edu.gmu.cs321.ApprovalScreen;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.paint.Color;
+
 public class ApprovalController extends LoginController {
     @FXML
     private TextArea appPetitionerInfo;
@@ -17,7 +19,7 @@ public class ApprovalController extends LoginController {
 
    // @FXML
     // Text
-
+    private ApprovalScreen approvalScreen = new ApprovalScreen();
 
    /**
     * Initialize should populate fields
@@ -28,7 +30,8 @@ public class ApprovalController extends LoginController {
     
     private void populateFields()
     {
-        appPetitionerInfo.setText("Petitioner:\nDOB\nEMAIL");
+
+        appPetitionerInfo.setText(approvalScreen.displayFields(approvalScreen.getApproval().getImmForm()));
         errorInfo.setText("Error Info Goes Here");
         approvalResult.setText("Approval message goes here");
     }
@@ -39,8 +42,21 @@ public class ApprovalController extends LoginController {
      */
     //@FXML
     public boolean onAcceptClick(ActionEvent event) {
+        String outputText = "";
+        boolean isApproved=false;
         approvalResult.setTextFill(Color.BLUE);
-        approvalResult.setText("Form was Accepted, Redunant Button!");
+
+        if (approvalScreen.getApproval().getImmForm()==null)
+            outputText = "No Form was approved!";
+        else
+            outputText= "Form was Accepted!";
+        
+        if (approvalScreen.approveForm()==true)
+            outputText= outputText.concat(" Displaying New Form!");
+        else
+            outputText = outputText.concat(" No new forms!");
+        approvalResult.setText(outputText);
+        appPetitionerInfo.setText(approvalScreen.displayFields(approvalScreen.getApproval().getImmForm()));
         return true;
     }
     /**
@@ -49,7 +65,8 @@ public class ApprovalController extends LoginController {
     @FXML
     public boolean onRejectClick(ActionEvent event) {
         approvalResult.setTextFill(Color.RED);
-        approvalResult.setText("Form was Denied, Sent to Reviewer");
+        approvalResult.setText(approvalScreen.rejectForm());
+        appPetitionerInfo.setText(approvalScreen.displayFields(approvalScreen.getApproval().getImmForm()));
         return true;
     }
     
