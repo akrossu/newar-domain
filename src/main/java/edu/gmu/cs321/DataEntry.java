01c59c03ea3a8fef3fde9dc16d117531830c440d
Akross <59298840+akrossu@users.noreply.github.com>
2024-11-19 03:34:57 -0500

    added workflow bridge

--- src/main/java/edu/gmu/cs321/DataEntry.java
+++ src/main/java/edu/gmu/cs321/DataEntry.java
@@ -1,34 +1,8 @@
 package edu.gmu.cs321;
-/**
- * Data Entry is used for inputed data
- */
-public class DataEntry {
-    /**
-     * object variables
-     */
-    private Boolean allFieldsFIlled;
-    private ImmForm ImmFormTemplate;
 
-    /**
-     * validate Fields
-     */
-    public int validateFields()
-    {
-        return -1;
-    }
-    /**
-     * get Form Template
-     * Currently returns a bool, but might return the form template later, tbd
-     */
-    public Boolean getFormTemplate()
-    {
-        return false;
-    }
-    /**
-     * add a worflow form
-     */
-    public Boolean addWorkflowForm()
-    {
-        return false;
+public class DataEntry {
+    public void addItemToWorkflow(ImmForm immForm) {
+        WorkflowStaging ws = new WorkflowStaging();
+        ws.createNewWFItem(immForm);
     }
 }
